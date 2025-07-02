eddfec4f18365a7e50e7cfecf5d7de42e1c57d9a
anthony-arseneau <anthonyfarseneau@icloud.com>
2025-07-02 17:24:30 -0300

    update

--- server.js
+++ server.js
@@ -114,6 +114,6 @@ app.post('/login', (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
