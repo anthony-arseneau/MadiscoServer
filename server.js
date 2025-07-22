49f7ea14d194a0c780d5628deeef51997faf4e28
anthony-arseneau <anthonyfarseneau@icloud.com>
2025-07-22 18:40:47 -0300

    update

--- server.js
+++ server.js
@@ -269,3 +269,7 @@ app.get("/", (req, res) => {
 https.createServer(options, app).listen(PORT, '0.0.0.0', () => {
   console.log(`HTTPS Server running on https://0.0.0.0:${PORT}`);
 });
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
