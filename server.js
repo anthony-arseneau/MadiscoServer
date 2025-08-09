184b2e0e96e062c02dc9852237af293c1c70f659
anthony-arseneau <anthonyfarseneau@icloud.com>
2025-08-09 12:17:19 -0300

    test

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 const PORT = 443;
 
+app.use(cors());
+app.use(bodyParser.json());
+
 const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/anthonyarseneau.ca/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/anthonyarseneau.ca/fullchain.pem')
